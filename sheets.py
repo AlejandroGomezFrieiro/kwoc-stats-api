6b225335e3e54390d2fd235471fe82671dbd02f3
Alejandro Gomez Frieiro <alejandrogomezfrieiro@gmail.com>
2020-10-08 11:53:49 +1000

    Added feedback column into sheets.py

--- sheets.py
+++ sheets.py
@@ -15,25 +15,27 @@ to_share_email = 'target_email@gmail.com'
 # Create a pandas dataframe with the csv file
 dataframe = pd.read_csv('students_.csv')
 
+# Fix problems with inadequate inputs (empty input in csv file gives a NaN on pandas)
 dataframe.fillna('', inplace=True)
-# Create title for the new columns
-# midevals_cleared_array = ['midevals_cleared']
-# endevals_cleared_array = ['endevals_cleared']
 
-
+# Set extra columns
 midevals_cleared_array = [0] * (len(dataframe.index))
 endevals_cleared_array = [0] * (len(dataframe.index))
+feedback = [''] * (len(dataframe.index))
 
 dataframe['midevals_cleared'] = midevals_cleared_array
 dataframe['endevals_cleared'] = endevals_cleared_array
+dataframe['feedback'] = feedback
 
 # Connect to your account using credentials. Obtain credentials following this guide "https://gspread.readthedocs.io/en/latest/oauth2.html#enable-api-access-for-a-project"
 gc = gs.service_account(filename='./env/api_credentials.json')
 
 # Create a new spreadsheet
 sh = gc.create('kwoc-stats-api')
+
 # Create worksheet with name Students
 worksheet = sh.add_worksheet(title="Students", rows=str(len(dataframe.index)), cols="5")
+
 # Populate worksheet with pandas dataframe
 worksheet.update([dataframe.columns.values.tolist()] + dataframe.values.tolist())
 
